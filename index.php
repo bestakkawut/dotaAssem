ada956fd68a4a48ad295cf3c881d4d9da8559b33
Akkawut <blf_forever@hotmail.co.th>
2014-03-23 19:56:53 +0700

    add all files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">        
+        <link href="css/resetCss.css" rel="stylesheet" >
+        <link href="css/index.css" rel="stylesheet" >
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ js/index_js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+});
+
+function first_load(){
+    $("#first-page").css({
+        display: 'none'
+    });
+}
